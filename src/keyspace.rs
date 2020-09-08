7eba6c5b1d8be9067ae8eec7e22e7cb11a65c38d
Aryailia <Aryailia@users.noreply.github.com>
2020-09-09 02:52:42 +0800

    dealing with lifetimes, fixing returning a keyspace

--- src/keyspace.rs
+++ src/keyspace.rs
@@ -1,6 +1,6 @@
-use std::borrow::Cow;
 use crate::constants::{Chord, Hotkey, Shortcut};
 use crate::parser::PermutationsGenerator;
+use std::borrow::Cow;
 
 // TODO: Remove this clone, maybe. Was used for prototyping
 #[derive(Clone, Debug)]
@@ -12,11 +12,83 @@ enum KeyspaceAction<'a, 'b> {
 // Convert from a list of Shorcuts to a list of states
 #[derive(Debug)]
 pub struct Keyspace<'a, 'b> {
-    activator: Hotkey<'b>,
+    // The path to this keyspace (does not do anything semantically)
+    title: Hotkey<'b>,
+    // The list of keys. While inside this keyspace, press 'Chord' to
+    // performan 'KeyspaceAction'
     list: Vec<(Chord, KeyspaceAction<'a, 'b>)>,
 }
 
+// As a 'trait' rather than direct 'impl' so we import explicitly
+pub trait KeyspaceList<'a> {
+    fn keyspace_list<'b>(&'b self) -> Result<Vec<Keyspace<'a, 'b>>, String>;
+}
+
+// This a means of providing window managers that do not support chaining
+// keyboard chords the means to do so (if there is a way to set state).
+//
+// Abstractly, this is takes the shortcut list and parses it into a tree where
+// the non-leaf nodes are single chords (where a path from root to leaf is
+// the shortcut hotkey) and the leaf nodes are the associated shortcut actions.
+//
+// Then flattens this tree into an associative array of Keyspace, which is
+// conceptually: state + Chord => action. A 'Keyspace' is essentially any
+// edge between two nodes in the tree.
+//
+// In implementation, we do not use a tree. Instead, we make use of
+// 'VisitedTracker' which is just a (usize, usize) of the same length as
+// the shortcut_list to track the traversal of the shortcut list
+impl<'a> KeyspaceList<'a> for PermutationsGenerator<'a> {
+    fn keyspace_list<'b>(&'b self) -> Result<Vec<Keyspace<'a, 'b>>, String> {
+        let shortcut_list = self.allocate_shortcut_list()?;
+        let mut keyspace_list = Vec::new();
+        let mut visited = VisitedTracker::new(shortcut_list.as_slice());
+        for (i, shortcut) in shortcut_list.iter().enumerate() {
+            let first_hotkey_chord_list = shortcut.hotkey.0;
+
+            // Loops through all chords in 'shortcut' and creating
+            // 1. a 'Keyspace' for every k but the last chord index
+            //    So [Ctrl + A; Ctrl + B; Ctrl + C] make two 'Keyspace''s
+            // 2. a new 'KeyspaceAction' for every pair (k, k + 1) for all k
+            // 3. Through 'visited', ensure we do not double process entries
+            //
+            // Continually narrow the containing partition by chord, 
+            for j in visited.unvisited_chord_indicies(i) {
+                // Build the keyspace
+                let mut space = Keyspace {
+                    title: Hotkey(&first_hotkey_chord_list[0..j]),
+                    list: vec![],
+                };
+                visited
+                    // Updates 'visited' so we know what has been processed
+                    // and also provides an iterator
+                    .refine_container_for_i_partitioning_by_chord_j(i, j)
+                    // Make the appropriate 'KeyspaceAction'
+                    .for_each(|partition| space.push_partition(partition, j));
+                keyspace_list.push(space);
+            }
+        }
+        Ok(keyspace_list)
+    }
+}
+
+
 impl<'a, 'b> Keyspace<'a, 'b> {
+    fn push_partition(&mut self, partition: &[Shortcut<'a, 'b>], chord_index: usize) {
+        if partition.len() >= 2 {
+            let chord_list = &partition[0].hotkey.0;
+            //println!("===\n{:?}\n{:?} {}\n", partition, chord_list, chord_index);
+            self.list.push((
+                chord_list[chord_index].clone(),
+                KeyspaceAction::SetState(&chord_list[0..chord_index + 1]),
+            ));
+        //keyspace.push_shortcut(&partition[0], 0);
+        } else {
+            debug_assert!(partition.len() > 0);
+            self.push_shortcut(&partition[0], chord_index);
+        }
+    }
+
     fn push_shortcut(&mut self, shortcut: &Shortcut<'a, 'b>, chord_index: usize) {
         let chord_list = shortcut.hotkey.0;
         if chord_index + 1 == chord_list.len() {
@@ -34,100 +106,15 @@ impl<'a, 'b> Keyspace<'a, 'b> {
         }
     }
 
-    fn push_partition(&mut self, partition: ShortcutPartition<'a, 'b>, chord_index: usize) {
-        if partition.len() >= 2 {
-            let chord_list = &partition[0].hotkey.0;
-            //println!("===\n{:?}\n{:?} {}\n", partition, chord_list, chord_index);
-            self.list.push((
-                chord_list[chord_index].clone(),
-                KeyspaceAction::SetState(&chord_list[0..chord_index + 1]),
-            ));
-            //keyspace.push_shortcut(&partition[0], 0);
-        } else {
-            debug_assert!(partition.len() > 0 );
-            self.push_shortcut(&partition[0], chord_index);
-        }
-    }
-
     fn print(&self) {
-        self.list.iter().for_each(|entry| {
-            println!("{}", entry.0);
-            match entry.1 {
-                KeyspaceAction::SetState(chord_list) =>
-                    println!("  Set: {}", Hotkey(chord_list)),
-                KeyspaceAction::Action(action) =>
-                    println!("  Action: {:?}", action.join("")),
+        println!("=== {} ===", self.title);
+        self.list.iter().for_each(|entry| match entry.1 {
+            KeyspaceAction::SetState(chord_list) => {
+                println!("{} Set: {}", entry.0, Hotkey(chord_list))
             }
+            KeyspaceAction::Action(action) => println!("{} Action: {:?}", entry.0, action.join("")),
         });
     }
-}
-
-// As a 'trait' rather than direct 'impl' so we import explicitly
-pub trait KeyspaceList {
-    fn keyspace_list(&self) -> Result<(), String>;
-}
-pub trait KeyspaceList2<'a> {
-    fn keyspace_list<'b>(&'b self) -> Result<Vec<Keyspace<'a, 'b>>, String>;
-}
-
-
-// Most shortcut terminal
-//impl<'a> KeyspaceList<'a> for PermutationsGenerator<'a> {
-impl<'a> KeyspaceList for PermutationsGenerator<'a> {
-    //fn keyspace_list<'b>(&'b self) -> Result<Vec<Keyspace<'a, 'b>>, String> {
-    fn keyspace_list(&self) -> Result<(), String> {
-        //use crate::constants::{Key, Mod};
-        let shortcut_list = self.allocate_shortcut_list()?;
-
-        let mut keyspace_list = Vec::new();
-        let mut visited = VisitedTracker::new(shortcut_list.as_slice());
-        shortcut_list.iter().enumerate().for_each(|(i, shortcut)| {
-            let head_chord_list = shortcut.hotkey.0;
-            for j in visited.last_unvisited_index(i)..head_chord_list.len() {
-                // Build the keyspace
-                let mut keyspace = Keyspace {
-                    activator: Hotkey(&head_chord_list[0..j]),
-                    list: vec![],
-                };
-                println!("{} {} {}", i, j, Hotkey(&head_chord_list[0..j]));
-                visited.refine_partition_of_i_by_chord_j(i, j).for_each(|partition| {
-                    //if i == 1 && j == 2 {
-                    //    println!("{:?}", visited.tracker);
-                    //}
-                    //println!("  {}", partition.len());
-                    keyspace.push_partition(partition, j);
-                });
-                keyspace_list.push(keyspace);
-            }
-        });
-        println!("\n---\n---\n");
-        //print_keyspace_list(&keyspace_list);
-        //print_keyspace_list2(&keyspace_list);
-        println!("{}", keyspace_list.len());
-        Ok(())
-        //Ok(keyspace_list)
-    }
-}
-
-fn print_keyspace_list<'a, 'b>(keyspace_list: &[Keyspace<'a, 'b>]) {
-    keyspace_list.iter().for_each(|keyspace| {
-        println!("==== {} ===", keyspace.activator);
-        keyspace.list.iter().for_each(|(chord, action)| {
-            match action {
-                KeyspaceAction::SetState(chord_list) =>
-                    println!("{}: set {}", chord, Hotkey(chord_list)),
-                KeyspaceAction::Action(a) =>
-                    println!("{}: {:?}", chord, a.join("")),
-            }
-        });
-    });
-}
-
-fn print_keyspace_list2<'a, 'b>(list: &[Keyspace<'a, 'b>]) {
-    list.iter().for_each(|keyspace| {
-        println!("==== {} ===", keyspace.activator);
-        keyspace.print();
-    });
 }
 
 fn print_shortcut_list<'a, 'b>(list: &[Shortcut<'a, 'b>]) {
@@ -137,76 +124,71 @@ fn print_shortcut_list<'a, 'b>(list: &[Shortcut<'a, 'b>]) {
     });
 }
 
-struct VisitedTracker<'a, 'b> {
-    shortcut_list: &'b [Shortcut<'a, 'b>],
-    // (visited_chord_index, container_partition_len)
+struct VisitedTracker<'a, 'b, 'c> {
+    shortcut_list: &'c [Shortcut<'a, 'b>],
+
+    // .0: index in 'shortcut_list[?].hotkey.0' after chords already processed
+    // .1: container_paritition_end (pos in 'shortcut_list')
     tracker: Vec<(usize, usize)>,
 }
 
-impl<'a, 'b> VisitedTracker<'a, 'b> {
-    fn new(shortcut_list: &'b [Shortcut<'a, 'b>]) -> Self {
+impl<'a, 'b, 'c> VisitedTracker<'a, 'b, 'c> {
+    fn new(shortcut_list: &'c [Shortcut<'a, 'b>]) -> Self {
         let len = shortcut_list.len();
         Self {
             shortcut_list,
             tracker: vec![(0, len); len],
         }
     }
-    fn last_unvisited_index(&self, index: usize) -> usize {
-        self.tracker[index].0
+
+    fn unvisited_chord_indicies(&self, index: usize) -> std::ops::Range<usize> {
+        let last_visited = self.tracker[index].0;
+        let total_chords = self.shortcut_list[index].hotkey.0.len();
+        last_visited..total_chords
     }
 
-    // Begins with whole 'self.shortcut_list'
-    fn refine_partition_of_i_by_chord_j(
+    //// Begins with whole 'self.shortcut_list'
+    fn refine_container_for_i_partitioning_by_chord_j(
         &mut self,
-        mut shortcut_index: usize,
-        chord_index: usize,
-    ) -> SharedChordWalker<'a, 'b> {
-        let end = self.tracker[shortcut_index].1;
+        shortcut_index: usize, // i
+        chord_index: usize,    // j
+    ) -> SharedChordPartitioner<'a, 'b, 'c> {
+        let close = self.tracker[shortcut_index].1;
+        let container_partition = &self.shortcut_list[shortcut_index..close];
 
-        let container_partition = &self.shortcut_list[shortcut_index..end];
+        let chunk_container_again_by_chord_j = SharedChordPartitioner {
+            list: container_partition,
+            chord_index,
+        };
+
         debug_assert!(container_partition.len() >= 1);
         debug_assert!(chord_index < container_partition[0].hotkey.0.len());
-        SharedChordWalker {
-            list: container_partition,
-            chord_index,
-        }.for_each(|partition| {
-            let partition_start = shortcut_index;
-            let partition_close = partition_start + partition.len();
-            let visited = (chord_index + 1, partition_close);
-            for i in 0..partition.len() {
-                self.tracker[partition_start + i] = visited;
-            }
-            shortcut_index += partition.len();
 
-        });
+        let mut index = shortcut_index;
+        chunk_container_again_by_chord_j
+            .clone()
+            .for_each(|partition| {
+                let subpartition_len = partition.len();
+                let subpartition_close = index + subpartition_len;
+                for _ in 0..subpartition_len {
+                    self.tracker[index] = (chord_index + 1, subpartition_close);
+                    index += 1;
+                }
+            });
 
-        SharedChordWalker {
-            list: container_partition,
-            chord_index,
-        }
+        chunk_container_again_by_chord_j
     }
 }
 
-type ShortcutPartition<'a, 'b> = &'b [Shortcut<'a, 'b>];
-
-fn split_by_shared_chord<'a, 'b>(list: &'b [Shortcut<'a, 'b>], chord_index: usize) -> SharedChordWalker<'a ,'b> {
-    debug_assert!(list.len() >= 1);
-    debug_assert!(chord_index < list[0].hotkey.0.len());
-
-    SharedChordWalker {
-        list,
-        chord_index,
-    }
-}
-
-struct SharedChordWalker<'a, 'b> {
-    list: &'b [Shortcut<'a,'b>],
+#[derive(Clone)]
+struct SharedChordPartitioner<'a, 'b, 'c> {
+    list: &'c [Shortcut<'a, 'b>],
     chord_index: usize,
 }
 
-impl<'a, 'b> Iterator for SharedChordWalker<'a, 'b> {
-    type Item = ShortcutPartition<'a, 'b>;
-    fn next(&mut self) -> Option::<Self::Item> {
+impl<'a, 'b, 'c> Iterator for SharedChordPartitioner<'a, 'b, 'c> {
+    type Item = &'c [Shortcut<'a, 'b>];
+    fn next(&mut self) -> Option<Self::Item> {
         if self.list.len() > 0 {
             let y = self.chord_index;
             let first_hotkey = self.list[0].hotkey.0;
@@ -215,7 +197,7 @@ impl<'a, 'b> Iterator for SharedChordWalker<'a, 'b> {
                 for shortcut in &self.list[1..] {
                     let hotkey = shortcut.hotkey.0;
                     if y >= hotkey.len() || &hotkey[y] != &first_hotkey[y] {
-                        break
+                        break;
                     }
                     close += 1;
                 }
@@ -230,4 +212,3 @@ impl<'a, 'b> Iterator for SharedChordWalker<'a, 'b> {
         }
     }
 }
-
